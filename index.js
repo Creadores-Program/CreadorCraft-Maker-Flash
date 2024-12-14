5c7e136718643b3283c8f8f43624fcfbe49ea292
Trollhunters501PC <cpcreador@gmail.com>
2024-12-13 20:14:58 -0600

    Test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const prefix = "[CreadorCraft Maker] ";
-console.info(prefix+"CreadorCraft Maker Action by Creadores Program ©2024");
+const prefix = "[CreadorCraft Maker Flash] ";
+console.info(prefix+"CreadorCraft Maker Flash by Creadores Program ©2024");
 console.info(prefix+"Loading Libraries...");
 try{
   const { execSync } = require("child_process");
@@ -7,96 +7,28 @@ try{
   execSync("npm install", { stdio: "inherit", cwd: rute });
   var core = require('@actions/core');
   var github = require('@actions/github');
-  var archiver = require('archiver');
   var fs = require("fs");
 }catch(error){
   console.error(error.stack || error.message);
   core.setFailed(error.stack || error.message);
 }
-//error Messages
-const errorMessages = {
-    inManifest: "\nError in manifest.json",
-    inFileNotFound: function(file){
-      return "\nError "+file+" Not Found";
-    }
-};
 console.info(prefix+"Done!");
 console.info(prefix+"Creating CreatorCraft Game...");
-var dirGame = core.getInput("path");
+var dirGame = core.getInput("pathSWF");
+var dirJS = core.getInput("path");
+var elementID = core.getInput("elementID");
 try{
-  var manifestCCG = {};
-  fs.readFile(dirGame+"/manifest.json", 'utf8', (err, data) => {
+  fs.readFile(__dirname.replaceAll("\\", "/")+"/org/CreadoresProgram/CreadorCraftMaker/Flash/script.js", "utf-8", function(err, data){
     if(err){
-        throw err;
+      throw err;
     }
-    manifestCCG = JSON.parse(data);
-    if(manifestCCG.name == null || manifestCCG.name.trim() == ""){
-        throw new Error(prefix+"You need a name for the game!"+errorMessages.inManifest);
-    }
-    if(manifestCCG.version == null || manifestCCG.version.trim() == ""){
-        throw new Error(prefix+"You need a version for the game!"+errorMessages.inManifest);
-    }
-    if(manifestCCG.description == null){
-        throw new Error(prefix+"You need a description for the game!"+errorMessages.inManifest);
-    }
-    if(((manifestCCG.description.trim().toLowerCase().indexOf("<script") != -1 || manifestCCG.description.trim().toLowerCase().indexOf("</script") != -1) || (manifestCCG.name.trim().toLowerCase().indexOf("<script") != -1 || manifestCCG.name.trim().toLowerCase().indexOf("</script") != -1)) || (manifestCCG.version.trim().toLowerCase().indexOf("<script") != -1 || manifestCCG.version.trim().toLowerCase().indexOf("</script") != -1)){
-      throw new Error(prefix+"Name, Version or Description Invalid!"+errorMessages.inManifest);
-    }
-    if(manifestCCG.mainHtml == null){
-        throw new Error(prefix+"You need a main Html for the game!"+errorMessages.inManifest);
-    }
-    if(manifestCCG.mainJS == null){
-        throw new Error(prefix+"You need a main JS for the game!"+errorMessages.inManifest);
-    }
-    function VerifyAccess(dirs) {
-      if(!fs.existsSync(dirGame+"/"+dirs)){
-        throw new Error(prefix+errorMessages.inFileNotFound(dirs));
+    fs.writeFile(dirJS+"/main.js", data.replaceAll("{elementID}", elementID).replaceAll("{FileSWF}", dirGame), 'utf8', (err2) => { 
+      if (err2) { 
+        console.error(prefix+'Convert Fail!');
+        throw err2;
       }
-    }
-    VerifyAccess(manifestCCG.mainHtml);
-    VerifyAccess(manifestCCG.mainJS);
-    if(manifestCCG.mainJSmodule != null) VerifyAccess(manifestCCG.mainJSmodule);
-    if(manifestCCG.mainCSS != null) VerifyAccess(manifestCCG.mainCSS);
-    if(manifestCCG.mainPython != null) VerifyAccess(manifestCCG.mainPython);
-    if(manifestCCG.mainWebAssembly != null) VerifyAccess(manifestCCG.mainWebAssembly);
-    if(manifestCCG.mainCoffeeScript != null) VerifyAccess(manifestCCG.mainCoffeeScript);
-    if(manifestCCG.mainLS != null) VerifyAccess(manifestCCG.mainLS);
-    if(manifestCCG.mainTS != null) VerifyAccess(manifestCCG.mainTS);
-    if(manifestCCG.mainLatinoScript != null) VerifyAccess(manifestCCG.mainLatinoScript);
-    if(manifestCCG.mainSCSS != null) VerifyAccess(manifestCCG.mainSCSS);
-    if(manifestCCG.mainPerl != null) VerifyAccess(manifestCCG.mainPerl);
-    if(manifestCCG.mainRuby != null) VerifyAccess(manifestCCG.mainRuby);
-    if(manifestCCG.mainLua != null) VerifyAccess(manifestCCG.mainLua);
-    if(manifestCCG.mainPHP != null) VerifyAccess(manifestCCG.mainPHP);
-    if(manifestCCG.mainLat != null) VerifyAccess(manifestCCG.mainLat);
-    console.info(prefix+"Verification Complete");
-    console.info(prefix+"Packing...");
-    function zipDirectory(sourceDir, outPath) {
-      const output = fs.createWriteStream(outPath);
-      const archive = archiver('zip', { zlib: { level: 9 } });
-    
-      output.on('close', () => {
-        console.info(prefix+"Game "+manifestCCG.name+" "+manifestCCG.version+".creadorcraftgame.zip Build Correctly in "+outPath);
-      });
-    
-      archive.on('error', (err) => {
-        console.error(prefix+"Game "+manifestCCG.name+" "+manifestCCG.version+".creadorcraftgame.zip Build Fail");
-        throw err;
-      });
-    
-      archive.pipe(output);
-      archive.directory(sourceDir, false);
-
-  archive.finalize();
-}
-
-  const sourceDir = dirGame;
-  if(!fs.existsSync("./gameBuildCCM")){
-    fs.mkdirSync("./gameBuildCCM");
-  }
-  const outPath = "./gameBuildCCM/"+manifestCCG.name+" "+manifestCCG.version+".creadorcraftgame.zip";
-
-  zipDirectory(sourceDir, outPath);
+      console.info(prefix+'Convert Correctly!'); 
+    });
   });
 }catch(error){
     console.error(error.message);
--- org/CreadoresProgram/CreadorCraftMaker/Flash/script.js
+++ org/CreadoresProgram/CreadorCraftMaker/Flash/script.js
@@ -1,3 +1,18 @@
 $.getScript("https://unpkg.com/@ruffle-rs/ruffle", function(){
-  window.addEventListener();
+  window.addEventListener("load", function(){
+    let ruffle = window.RufflePlayer.newest();
+    let player = ruffle.createPlayer();
+    let container = document.getElementById("{elementID}");
+    container.appendChild(player);
+    GameProps.getFileGame().file("{FileSWF}").async("unit8array").then(function(GFlashB){
+      let GFlashBl = new Blob([GFlashB], {
+        type: "application/x-shockwave-flash"
+      });
+      let GFlashUrl = URL.createObjectURL(GFlashBl);
+      player.ruffle().load(GFlashUrl).catch(function(error){
+        alert("Error Load Game!"+error);
+        GameProps.exitGame();
+      });
+    });
+  });
 });
